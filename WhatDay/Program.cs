0e8d79c240e1f1f4e291d02cdb21ec32a397a49e
Jolenebean <46964006+Jolenebean@users.noreply.github.com>
2019-01-23 22:00:26 +0800

    Add files via upload

---
+++ WhatDay/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("When is your Birthday?");
+            String oDate = Console.ReadLine();
+            DateTime nDate = DateTime.Parse(oDate);
+            Console.WriteLine("You are born on a {0}", nDate.DayOfWeek);
+            Console.Read(); 
+
+        }
+    }
+}
